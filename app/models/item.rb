389ac069d90097b698ce0f4552883900df629bac
豊瀬雅也 <toyosemasaya@yutakasayanoAir.elecom>
2021-08-27 13:14:48 +0900

    バリデーションの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,15 +4,15 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :title
-    validates :product_condition_id
-    validates :postage_id
-    validates :prefecture_id
-    validates :delivery_date_id
-    validates :category_id
-    validates :item_info
     validates :image
     with_options numericality: { with: /\A[0-9]+\z/, message: "Half-width number" }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" } do
       validates :price
     end
   end
+  validates :product_condition_id
+  validates :postage_id, numericality: { other_than: 0, message: "Select" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "Select" }
+  validates :delivery_date_id, numericality: { other_than: 0, message: "Select" }
+  validates :category_id, numericality: { other_than: 0, message: "Select" }
+  validates :item_info, numericality: { other_than: 0, message: "Select" }
 end
