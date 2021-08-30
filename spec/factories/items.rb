b7ec9a271c4dc0229b031c8c67ad90403b0695ed
豊瀬雅也 <toyosemasaya@yutakasayanoAir.elecom>
2021-08-30 21:00:47 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    image         {Faker::Lorem.sentence}
+    title                        {"名前"}
+    item_info                    {"説明"}
+    price                        {2000}
+    category_id                  {1}
+    product_condition_id         {1}
+    postage_id                   {1}
+    prefecture_id                {1}
+    delivery_date_id             {1}
+    association :user
+
+    after(:build) do |item|
+    item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
