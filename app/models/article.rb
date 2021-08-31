edcf94741b3c69e9295b4d80880aeb5ea415468b
豊瀬雅也 <toyosemasaya@yutakasayanoAir.elecom>
2021-08-31 11:47:36 +0900

    articleの削除

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,3 @@
 class Article < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :product_condition
-  belongs_to :postage
-  belongs_to :prefecture
-  belongs_to :delivery_date
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/new'
   devise_for :users
   root to: "items#index"
-  resources :articles
   resources :items, only: [:new, :create]
 end
