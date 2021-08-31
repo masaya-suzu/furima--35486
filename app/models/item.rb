79f6ac694181d593d46203a082bd58ac1d37e4d4
豊瀬雅也 <toyosemasaya@yutakasayanoAir.elecom>
2021-08-31 11:50:33 +0900

    モデルの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :product_condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :delivery_date
+
 
   with_options presence: true do
     validates :title
-    validates :image
     with_options numericality: { with: /\A[0-9]+\z/, message: "Half-width number" }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range" } do
       validates :price
     end
