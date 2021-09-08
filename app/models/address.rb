99236321b0050de7edfbf81b0cf25ec95bad7d99
豊瀬雅也 <toyosemasaya@yutakasayanoAir.elecom>
2021-09-08 19:16:41 +0900

    アドレスモデルのバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :buyer
+
+  validates :postcode, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, numericality: {other_than: 0, message: "Select"}
+  validates :city, presence: true
+  validates :block, presence: true
+  validates :building, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10}$|^\d{11}\z/ }
 end
