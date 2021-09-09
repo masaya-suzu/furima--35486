cbc70d0df678b7cab1608d552acb1d3abb47d12f
豊瀬雅也 <toyosemasaya@yutakasayanoAir.elecom>
2021-09-09 15:18:19 +0900

    バリデーション処理の記述場所の変更

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,23 +1,30 @@
 class BuyersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index]
+  #before_action :sold_out_item, only: [:index]
+
   def index
     @item = Item.find(params[:item_id])
-    @buyer = Buyer.new
+    @buyer_address = BuyerAddress.new
   end
 
   def create
-    @buyer = Buyer.create(buyer_params)
-    Address.create(address_params)
-    redirect_to root_path
+    @item = Item.find(params[:item_id])
+    @buyer_address = BuyerAddress.new(buyer_params)
+    if @buyer_address.valid?
+      @buyer_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
 
   def buyer_params
-    params.permit(:price).merge(user_id: current_user.id)
+    params.require(:buyer_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id)
   end
 
-  def address_params
-    params.permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(buyer_id: @buyer.id)
-  end
+ # def sold_out_item
+  #  redirect_to root_path if @item.purchase.present?
+  # end
 end
--- app/models/buyer_address.rb
+++ app/models/buyer_address.rb
@@ -1,4 +1,15 @@
 class BuyerAddress < ApplicationRecord
   include ActiveModel::Model
   attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number
+
+  validates :postcode, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, numericality: {other_than: 0, message: "Select"}
+  validates :city, presence: true
+  validates :block, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d{10}$|^\d{11}\z/ }
+
+  def save
+    buyer = Buyer.create(user_id: user_id, item_id: item_id)
+    Address.create(postcode: postcode, prefecture: prefecture, city: city, block: block, building: building, buyer_id: buyer.id)
+  end
 end
