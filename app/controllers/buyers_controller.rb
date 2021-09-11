40204fc9546b0353b07a28db7520371a7ffc00d2
豊瀬雅也 <toyosemasaya@yutakasayanoAir.elecom>
2021-09-11 16:26:48 +0900

    sold out の設定

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,24 +1,20 @@
 class BuyersController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, only: [:index]
   #before_action :sold_out_item, only: [:index]
 
   def index
     @item = Item.find(params[:item_id])
     @buyer_address = BuyerAddress.new
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 
   def create
-    #binding.pry
     @item = Item.find(params[:item_id])
     @buyer_address = BuyerAddress.new(buyer_params)
-    #binding.pry
     if @buyer_address.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item[:price],
-        card: buyer_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @buyer_address.save
       redirect_to root_path
     else
@@ -32,7 +28,13 @@ class BuyersController < ApplicationController
     params.require(:buyer_address).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
- # def sold_out_item
-  #  redirect_to root_path if @item.purchase.present?
-  # end
+  def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item[:price],
+        card: buyer_params[:token],
+        currency: 'jpy'
+      )
+  end
+
 end
