4b69326ca63bd2d45b19287d6d4b2d5558796828
豊瀬雅也 <toyosemasaya@yutakasayanoAir.elecom>
2021-09-12 13:01:12 +0900

    修正５

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.buyer || current_user
+    if @item.buyer || current_user.id == item.find(params[:id]).user_id
       redirect_to root_path
     end
   end
